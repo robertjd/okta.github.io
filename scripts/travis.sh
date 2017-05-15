b7412cbd516728dc7883da3ad661cee323b3e357
Vijet <vijet.mahabaleshwar@okta.com>
2017-05-15 15:40:48 +0000

    Removing sudo

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -26,11 +26,11 @@ if [ "$CHROMEHEADLESS" == "true" ]; then
   cat /etc/*release
 
   # Install latest stable version of google chrome
-  sudo apt-get install libxss1 libappindicator1 libindicator7
+  apt-get install libxss1 libappindicator1 libindicator7
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  sudo dpkg -i google-chrome*.deb
-  sudo apt-get install -f
-  
+  dpkg -i google-chrome*.deb
+  apt-get install -f
+
   google-chrome --version
 fi
 
