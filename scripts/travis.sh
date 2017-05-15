643df992f0d9b8deaf07cfdd74073550ec20ee10
Vijet <vijet.mahabaleshwar@okta.com>
2017-05-15 15:33:19 +0000

    Adding install command for chrome on debian

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -26,8 +26,11 @@ if [ "$CHROMEHEADLESS" == "true" ]; then
   cat /etc/*release
 
   # Install latest stable version of google chrome
-  chmod u+x ./scripts/install_chrome.sh
-  ./scripts/install_chrome.sh -f
+  sudo apt-get install libxss1 libappindicator1 libindicator7
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome*.deb
+  sudo apt-get install -f
+  
   google-chrome --version
 fi
 
