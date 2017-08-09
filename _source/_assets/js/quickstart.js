014d493f503a2255ab6f249796e08fa9cab1a62e
Robert Damphousse <robert.damphousse.devex@okta.com>
2017-08-09 08:44:37 -0700

    almost there

--- _source/_assets/js/quickstart.js
+++ _source/_assets/js/quickstart.js
@@ -1,135 +1,119 @@
-//= require vendor/director-1.2.6.min.js
-
-  // links working
-  // default content
-  // icons
+/**
+ * linkState
+ *
+ * This object defines the links that will appear, and serves as a state object once
+ * the app begins.  If you need to add new libraries, add them to this object.  If
+ * you need to change the default active libraries, change the active states in this
+ * object.
+ *
+ * `active` links will be selected by default. As the user navigates, the active
+ * properties will be modified accordingly.
+ */
+var linkState = {
+  clients: [
+    {
+      name: 'angular',
+      label: 'Angular',
+      serverExampleType: 'implicit'
+    },
+    {
+      name: 'ios',
+      label: 'iOS',
+      serverExampleType: 'implicit'
+    },
+    {
+      name: 'widget',
+      label: 'Sign-In Widget',
+      serverExampleType: 'auth-code',
+      active: true
+    }
+  ],
+  servers: [
+    {
+      active: true,
+      name: 'node',
+      label: 'Node JS',
+      frameworks: [
+        {
+          active: true,
+          name: 'generic',
+          label: 'Generic Node'
+        },
+        {
+          name: 'express',
+          label: 'Express.js'
+        }
+      ]
+    },
+    {
+      name: 'java',
+      label: 'Java',
+      frameworks: [
+        {
+          name: 'generic',
+          label: 'Generic Java',
+          active: true
+        },
+        {
+          name: 'spring',
+          label: 'Spring'
+        }
+      ]
+    },
+    {
+      name: 'php',
+      label: 'PHP',
+      frameworks: [
+        {
+          name: 'generic',
+          label: 'Generic PHP',
+          active: true
+        }
+      ]
+    }
+  ]
+}
 
 /**
- * need to have implicit vs code template for each server/framework choice
+ * Matches /client/server/framework
  */
+var urlExpression = '/([^\/]*)?\/?([^\/]*)?\/?([^\/]*)';
 
-  var authCodeClients = ['ios', 'android'];
-  var implicitClients = ['widget', 'angular', 'react'];
-  var serverLanguages = ['node','java'];
-  var defaultClient = 'widget';
-  var defaultServer = 'node';
-  var defaultFramework = 'generic';
-
-  var knownClients = new RegExp(implicitClients.concat(authCodeClients).join('|'));
-
-  var linkDefinitions = {
-    clients: [
-      {
-        name: 'angular',
-        label: 'Angular'
-      },
-      {
-        name: 'ios',
-        label: 'iOS'
-      },
-      {
-        name: 'widget',
-        label: 'Sign-In Widget',
-        active: true
-      }
-    ],
-    servers: [
-      {
-        active: true,
-        name: 'node',
-        label: 'Node JS',
-        frameworks: [
-          {
-            active: true,
-            name: 'generic',
-            label: 'Generic Node'
-          },
-          {
-            name: 'express',
-            label: 'Express.js'
-          }
-        ]
-      },
-      {
-        name: 'java',
-        label: 'Java',
-        frameworks: [
-          {
-            name: 'generic',
-            label: 'Generic Java',
-            active: true
-          },
-          {
-            name: 'spring',
-            label: 'Spring'
-          }
-        ]
-      },
-      {
-        name: 'php',
-        label: 'PHP',
-        frameworks: [
-          {
-            name: 'generic',
-            label: 'Generic PHP',
-            active: true
-          }
-        ]
-      }
-    ]
-  }
-
-  function applyCurrentUrl() {
-    var matches = window.location.hash.match('/([^\/]*)?\/?([^\/]*)?\/?([^\/]*)');
-    if (matches && matches.length) {
-      var activeClient = matches[1];
-      var activeServer = matches[2];
-      var activeFramework = matches[3];
-      linkDefinitions.clients.forEach(function (client) {
-        client.active = client.name === activeClient;
-      });
-      linkDefinitions.servers.forEach(function (server) {
-        server.active = server.name === activeServer;
-        server.frameworks.forEach(function (framework) {
-          framework.active = framework.name === activeFramework;
-        });
-      });
-    }
-  }
-
-  applyCurrentUrl();
-
-  linkDefinitions.clients.forEach(function (client) {
+/**
+ * Iterate through the link state, render buttons and setup click handlers
+ */
+function renderLinks() {
+  linkState.clients.forEach(function (client) {
     var link = $('<a>', {
       text: client.label,
       class: client.active ? 'active' : '',
       click: function () {
-        linkDefinitions.clients.forEach(function (client) {
+        linkState.clients.forEach(function (client) {
           client.link.removeClass('active');
           client.active = false;
         });
         $(this).addClass('active');
         client.active = true;
-        setUrl();
+        applySelectionTuple(getSelectionTupleFromLinkSate());
       }
     });
     client.link = link;
     $('#client-selector').append(link);
   });
 
-  linkDefinitions.servers.forEach(function (server) {
+  linkState.servers.forEach(function (server) {
     var link = $('<a>', {
       text: server.label,
       class: server.active ? 'active' : '',
       click: function () {
-        linkDefinitions.servers.forEach(function (server) {
+        linkState.servers.forEach(function (server) {
           server.link.removeClass('active');
-           server.active = false;
+            server.active = false;
         });
         renderFrameworkLinks(server);
         $(this).addClass('active');
         server.active = true;
-        setUrl();
+        applySelectionTuple(getSelectionTupleFromLinkSate());
       }
     });
     server.link = link;
@@ -155,91 +139,131 @@
           });
           $(this).addClass('active');
           framework.active = true;
-          setUrl();
+          applySelectionTuple(getSelectionTupleFromLinkSate());
         }
       });
       framework.link = link;
       $('#framework-selector').append(link);
     });
   }
+}
 
-  function setUrl() {
-    var hash = '#/';
-    linkDefinitions.clients.forEach(function (client) {
-      if (client.active) {
-        hash += client.name + '/';
-      }
-    });
-    linkDefinitions.servers.forEach(function (server) {
-      if (server.active) {
-        hash += server.name + '/';
-        server.frameworks.forEach(function (framework) {
-          if (framework.active) {
-            hash += framework.name;
-          }
-        });
-      }
-    });
+/**
+ * Fetches the content for a given selection tuple
+ */
+function loadContent(client, server, framework) {
+  var clientContentUrl = client + '/default-example.html';
+  var serverContentUrl = server + '/' + framework + '-' + client.serverExampleType + '.html';
 
-    window.location.replace(hash);
-  }
+  $.ajax({
+    url: clientContentUrl
+  }).done(function( html ) {
+    $( "#client_content" ).html( html );
+  });
+  $.ajax({
+    url: serverContentUrl
+  }).done(function( html ) {
+    $( "#server_content" ).html( html );
+  });
+};
 
+/**
+ * Get the active selections from the window hash
+ *
+ * @returns [client, server, framework]
+ */
+function getSelectionTupleFromHash() {
+  var matches = window.location.hash.match(urlExpression);
+  var client = matches && matches[1];
+  var server = matches && matches[2];
+  var framework = matches && matches[3];
+  return [client, server, framework];
+}
 
-  if (!window.location.hash || !knownClients.test(window.location.hash)) {
-    setUrl();
-  }
-
-  var routes = {
-    '/([^\/]*)?\/?([^\/]*)?\/?([^\/]*)': function(client, server, framework) {
-
-      if (!client) {
-        client = defaultClient
-      }
-
-      if (!server) {
-        server = defaultServer;
-      }
-
-      if (!framework) {
-        framework = defaultFramework;
-      }
-
-      var clientContentUrl = client + '/default-example.html';
-
-      var serverExampleType = implicitClients.indexOf(client) > -1 ? 'implicit' : 'auth-code';
-
-      var serverContentUrl = server + '/' + framework + '-' + serverExampleType + '.html';
-
-      $.ajax({
-        url: clientContentUrl
-      }).done(function( html ) {
-        $( "#client_content" ).html( html );
-      });
-
-      $.ajax({
-        url: serverContentUrl
-      }).done(function( html ) {
-        $( "#server_content" ).html( html );
-      });
-
+/**
+ * Get the active selections from the link sate
+ *
+ * @returns [client, server, framework]
+ */
+function getSelectionTupleFromLinkSate() {
+  var tuple = [];
+  linkState.clients.forEach(function (client) {
+    if (client.active) {
+      tuple.push(client.name);
     }
-  };
+  });
+  linkState.servers.forEach(function (server) {
+    if (server.active) {
+      tuple.push(server.name)
+      server.frameworks.forEach(function (framework) {
+        if (framework.active) {
+          tuple.push(framework.name)
+        }
+      });
+    }
+  });
+  return tuple;
+}
 
-  window.scrollToServer = function () {
-    $('body').animate({scrollTop: $('#server_setup').offset().top - 150});
-    $('#server_setup_link').addClass('active');
-    $('#client_setup_link').removeClass('active');
-  };
+/**
+ * Apply a selection tuple to the application state (set new window hash and load content)
+ */
+function applySelectionTuple(selectionTuple) {
+  window.location.replace('#/' + selectionTuple.join('/'));
+  loadContent.apply(null, selectionTuple);
+}
 
-  window.scrollToClient = function () {
-    $('body').animate({scrollTop: $('#client_setup').offset().top - 150});
-    $('#client_setup_link').addClass('active');
-    $('#server_setup_link').removeClass('active');
-  };
+/**
+ * Apply a selection tuple to the link state (will modify `active` properties)
+ */
+function applySelectionTupleToLinkSate(selectionTuple) {
+  var activeClient = selectionTuple[0];
+  var activeServer = selectionTuple[1];
+  var activeFramework = selectionTuple[2];
 
-  var router = Router(routes);
+  linkState.clients.forEach(function (client) {
+    client.active = client.name === activeClient;
+  });
+  linkState.servers.forEach(function (server) {
+    server.active = server.name === activeServer;
+    server.frameworks.forEach(function (framework) {
+      framework.active = framework.name === activeFramework;
+    });
+  });
+}
 
-  router.init();
 
+/**
+ * Begin application.  Fetch default selections from link state, and current
+ * selections from window hash.  Window hash overrides default link state.
+ */
+function main() {
+  var hashTuple = getSelectionTupleFromHash();
+  var stateTuple = getSelectionTupleFromLinkSate();
+  var initialTuple = [
+    hashTuple[0] || stateTuple[0],
+    hashTuple[1] || stateTuple[1],
+    hashTuple[2] || stateTuple[2]
+  ];
+
+  applySelectionTupleToLinkSate(initialTuple);
+  renderLinks();
+  applySelectionTuple(initialTuple);
   $('#client_setup_link').addClass('active');
+}
 
+// Used to scroll to the right place without anchors, 150 is to account for our header space
+window.scrollToServer = function () {
+  $('body').animate({scrollTop: $('#server_setup').offset().top - 150});
+  $('#server_setup_link').addClass('active');
+  $('#client_setup_link').removeClass('active');
+};
+window.scrollToClient = function () {
+  $('body').animate({scrollTop: $('#client_setup').offset().top - 150});
+  $('#client_setup_link').addClass('active');
+  $('#server_setup_link').removeClass('active');
+};
+
+
+
+main();
